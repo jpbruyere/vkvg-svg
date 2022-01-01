e201a27fdc1e576a75f1bda71a7e85143e7a078e
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2022-01-01 07:22:41 +0100

    vkvg_svg_internal.h

--- src/vkvg_svg.c
+++ src/vkvg_svg.c
@@ -1,226 +1,15 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <errno.h>
-#include <stdint.h>
-#include "vkengine.h"
-#include <stdarg.h>
-#include <ctype.h>
+
 
 #include "vkvg_svg.h"
 #include "vkvg_extensions.h"
 
-
-
-#define DEBUG_LOG 1
-
-#ifdef DEBUG_LOG
-#define LOG(...) fprintf (stdout, __VA_ARGS__)
-#else
-#define LOG
-#endif
-
-static int res;
-
-typedef enum {
-	svg_element_type_linear_gradient,
-	svg_element_type_radial_gradient
-}svg_element_type;
-
-typedef enum {
-	svg_paint_type_none,
-	svg_paint_type_solid,
-	svg_paint_type_pattern,
-}svg_paint_type;
-
 #define ARRAY_INIT	8
 #define ARRAY_ELEMENT_TYPE void*
+
 #define ARRAY_IMPLEMENTATION
 #include "array.h"
 
-
-typedef enum {
-	svg_unit_cm,
-	svg_unit_mm,
-	svg_unit_Q,
-	svg_unit_in,
-	svg_unit_pt,
-	svg_unit_pc,
-	svg_unit_px,
-	svg_unit_percentage,
-}svg_unit;
-typedef enum {
-	svg_gradient_unit_objectBoundingBox,
-	svg_gradient_unit_userSpaceOnUse
-}svg_gradient_unit;
-
-typedef struct {
-	float number;
-	svg_unit units;
-} svg_length_or_percentage;
-typedef struct {
-	uint32_t hash;
-	svg_element_type type;
-} svg_element_id;
-
-typedef struct {
-	svg_element_id id;
-	svg_gradient_unit gradientUnits;
-	svg_length_or_percentage cx;
-	svg_length_or_percentage cy;
-	svg_length_or_percentage fx;
-	svg_length_or_percentage fy;
-	svg_length_or_percentage r;
-	VkvgPattern pattern;
-}svg_element_radial_gradient;
-typedef struct {
-	svg_element_id id;
-	svg_gradient_unit gradientUnits;
-	svg_length_or_percentage x1;
-	svg_length_or_percentage x2;
-	svg_length_or_percentage y1;
-	svg_length_or_percentage y2;
-	VkvgPattern pattern;
-}svg_element_linear_gradient;
-
-uint32_t _get_element_hash (void* elt) {
-	return ((svg_element_id*)elt)->hash;
-}
-svg_element_type _get_element_type (void* elt) {
-	return ((svg_element_id*)elt)->type;
-}
-typedef struct {
-	float x;
-	float y;
-	float w;
-	float h;
-} svg_viewbox;
-
-typedef struct {
-	char		elt[128];
-	char		att[1024];
-	char		value[1024*1024];
-	VkvgDevice	dev;
-	VkvgContext	ctx;
-	VkvgSurface surf;
-	uint32_t	width;//force surface width & height
-	uint32_t	height;
-	svg_viewbox viewBox;
-	bool		preserveAspectRatio;
-	bool		skip;//skip tag and children
-	uint32_t	currentIdHash;
-	array_t*	idList;
-	/*long		currentTagStartPos;*/
-
-} svg_context;
-
-enum prevCmd {none, quad, cubic};
-
-int read_tag (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill);
-
-#define get_attribute fscanf(f, " %[^=>]=%*[\"']%[^\"']%*[\"']", svg->att, svg->value)
-
-#define read_tag_end \
-	if (res < 0) {												\
-		LOG("error parsing: %s\n", svg->att);					\
-		return -1;												\
-	}															\
-	if (res == 1) {												\
-		if (getc(f) != '>') {									\
-			LOG("parsing error, expecting '>'\n");				\
-			return -1;											\
-		}														\
-		return 0;												\
-	}															\
-	if (getc(f) != '>') {										\
-		LOG("parsing error, expecting '>'\n");					\
-		return -1;												\
-	}
-
-int skip_attributes_and_children (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill);
-
-#define read_element_start										\
-	res = fscanf(f, " <%[^> \n\r]", svg->elt);					\
-	if (res < 0)												\
-		return 0;												\
-	if (!res) {													\
-		res = fscanf(f, "%*[^<]<%[^> \n\r]", svg->elt);			\
-		if (!res) {												\
-			LOG("element name parsing error (%s)\n", svg->elt);	\
-			return -1;											\
-		}														\
-	}															\
-	if (svg->elt[0] == '/') {									\
-		if (getc(f) != '>') {									\
-			LOG("parsing error, expecting '>'\n");				\
-			return -1;											\
-		}														\
-		return 0;												\
-	}															\
-	if (svg->elt[0] == '!') {									\
-		if (!strcmp(svg->elt,"!DOCTYPE")) {						\
-			while (!feof (f))									\
-				if (fgetc(f)=='>')								\
-					break;										\
-		} if (!strncmp(svg->elt, "!--", 3)) {					\
-			while (!feof (f)) {									\
-				if (fgetc(f)=='-') {							\
-					if (fgetc(f)=='-' && fgetc(f)=='>')			\
-						break;									\
-					else										\
-						fseek (f, -1, SEEK_CUR);				\
-				}												\
-			}													\
-		}														\
-		continue;												\
-	}															\
-	if (svg->skip) {											\
-		res = skip_attributes_and_children (svg, f, hasStroke, hasFill, stroke, fill);\
-		continue;												\
-	}															\
-
-#define skip_element {																\
-	svg->skip = true;																\
-	res = skip_attributes_and_children (svg, f, hasStroke, hasFill, stroke, fill);	\
-	svg->skip = false;																\
-}
-
-#define read_attributes_loop_start_lite \
-	while (style || res > 0) {														\
-		if (!style && res == 1 && svg->att[0] == '/')								\
-			break;																	\
-		if (!strcasecmp(svg->att, "style")) {										\
-			int styleLength = strlen (svg->value);									\
-			if (styleLength > 0) {													\
-				style = (char*)malloc(styleLength);									\
-				memccpy(style, svg->value, 0, styleLength);							\
-				pfStyle = fmemopen(style, styleLength, "r");						\
-				res = fscanf(pfStyle, " %[^:]:%[^;];", svg->att, svg->value);		\
-				continue;															\
-			}																		\
-		}																			\
-
-#define read_attributes_loop_start \
-	FILE* pfStyle = NULL;															\
-	char* style = NULL;																\
-	res = get_attribute;															\
-	read_attributes_loop_start_lite
-
-#define read_attributes_loop_end \
-	if (style) {																	\
-		if (feof(pfStyle)) {														\
-			fclose (pfStyle);														\
-			free (style);															\
-			style = NULL;															\
-		} else {																	\
-			res = fscanf(pfStyle, " %[^:]:%[^;];", svg->att, svg->value);			\
-			continue;																\
-		}																			\
-	}																				\
-	res = get_attribute;															\
-}
+#include "vkvg_svg_internal.h"
 
 uint32_t hash_string(const char * s)
 {
@@ -858,7 +647,7 @@ bool parse_floats (FILE* f, int floatCount, ...) {
 }
 bool try_parse_lenghtOrPercentage (const char* measure, float* number, svg_unit* units) {
 	char str_units[16];
-	res = sscanf(measure, "%f%[^, ]", number, str_units);
+	int res = sscanf(measure, "%f%[^, ]", number, str_units);
 	if (res == 0)
 		return false;
 	if (res == 2) {
@@ -929,7 +718,7 @@ bool try_parse_lenghtOrPercentage (const char* measure, float* number, svg_unit*
 
 bool try_parse_lenghtOrPercentage2 (const char* measure, svg_length_or_percentage* lop) {
 	char str_units[16];
-	res = sscanf(measure, "%f%[^, ]", &lop->number, str_units);
+	int res = sscanf(measure, "%f%[^, ]", &lop->number, str_units);
 	if (res == 0)
 		return false;
 	if (res == 2) {
@@ -1000,7 +789,7 @@ bool try_parse_lenghtOrPercentage2 (const char* measure, svg_length_or_percentag
 bool try_parse_color (const char* colorString, svg_paint_type* isEnabled, uint32_t* colorValue) {
 	if (colorString[0] == '#') {
 		char color[7];
-		res = sscanf(&colorString[1], " %[0-9A-Fa-f]", color);
+		int res = sscanf(&colorString[1], " %[0-9A-Fa-f]", color);
 		char hexColorString[12];
 		if (strlen(color) == 3)
 			sprintf(hexColorString, "0xff%c%c%c%c%c%c", color[2],color[2],color[1],color[1],color[0],color[0]);
@@ -1056,7 +845,6 @@ float _get_pixel_coord (float reference, svg_length_or_percentage* lop) {
 }
 void set_pattern (svg_context* svg, uint32_t patternHash) {
 	void* elt;
-	vkvg_pattern_type_t pat_type;
 	VkvgPattern pat;
 	if (try_find_by_id(svg, patternHash, &elt)) {
 		switch (_get_element_type(elt)) {
@@ -1840,6 +1628,7 @@ int read_stop_attributes (svg_context* svg, FILE* f, VkvgPattern pat, svg_paint_
 	return read_tag(svg, f, hasStroke, hasFill, stroke, fill);
 }
 int read_gradient_children (svg_context* svg, FILE* f, VkvgPattern pat,  svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill) {
+	int res = 0;
 	while (!feof (f)) {
 		read_element_start
 		if (!strcasecmp(svg->elt,"stop"))
@@ -2004,6 +1793,7 @@ int read_lineargradient_attributes (svg_context* svg, FILE* f, svg_paint_type ha
 	return read_gradient_children (svg, f, rg->pattern, hasStroke, hasFill, stroke, fill);
 }
 int read_defs_children (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill) {
+	int res = 0;
 	while (!feof (f)) {
 		read_element_start
 		if (!strcasecmp (svg->elt, "lineargradient"))
@@ -2030,7 +1820,7 @@ int read_defs_attributes (svg_context* svg, FILE* f, svg_paint_type hasStroke, s
 
 }
 int skip_attributes_and_children (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill) {
-	res = get_attribute;
+	int res = get_attribute;
 	while (res > 0) {
 		if (res == 1 && (svg->att[0] == '/' || svg->att[0] == '?'))
 			break;//self closing tag
@@ -2040,7 +1830,7 @@ int skip_attributes_and_children (svg_context* svg, FILE* f, svg_paint_type hasS
 	return read_tag(svg, f, hasStroke, hasFill, stroke, fill);
 }
 int read_tag (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill) {
-
+	int res = 0;
 	while (!feof (f)) {
 
 		read_element_start
---
+++ src/vkvg_svg_internal.h
@@ -0,0 +1,221 @@
+#ifndef VKVG_SVG_INTERNAL_H
+#define VKVG_SVG_INTERNAL_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+#include <stdint.h>
+#include "vkengine.h"
+#include <stdarg.h>
+#include <ctype.h>
+
+#include "vkvg.h"
+
+#define ARRAY_INIT	8
+#define ARRAY_ELEMENT_TYPE void*
+#include "array.h"
+
+#define DEBUG_LOG 1
+
+#ifdef DEBUG_LOG
+#define LOG(...) fprintf (stdout, __VA_ARGS__)
+#else
+#define LOG
+#endif
+
+typedef enum {
+	svg_element_type_linear_gradient,
+	svg_element_type_radial_gradient
+}svg_element_type;
+
+typedef enum {
+	svg_paint_type_none,
+	svg_paint_type_solid,
+	svg_paint_type_pattern,
+}svg_paint_type;
+
+typedef enum {
+	svg_unit_cm,
+	svg_unit_mm,
+	svg_unit_Q,
+	svg_unit_in,
+	svg_unit_pt,
+	svg_unit_pc,
+	svg_unit_px,
+	svg_unit_percentage,
+}svg_unit;
+typedef enum {
+	svg_gradient_unit_objectBoundingBox,
+	svg_gradient_unit_userSpaceOnUse
+}svg_gradient_unit;
+
+typedef struct {
+	float number;
+	svg_unit units;
+} svg_length_or_percentage;
+typedef struct {
+	uint32_t hash;
+	svg_element_type type;
+} svg_element_id;
+
+typedef struct {
+	svg_element_id id;
+	svg_gradient_unit gradientUnits;
+	svg_length_or_percentage cx;
+	svg_length_or_percentage cy;
+	svg_length_or_percentage fx;
+	svg_length_or_percentage fy;
+	svg_length_or_percentage r;
+	VkvgPattern pattern;
+}svg_element_radial_gradient;
+typedef struct {
+	svg_element_id id;
+	svg_gradient_unit gradientUnits;
+	svg_length_or_percentage x1;
+	svg_length_or_percentage x2;
+	svg_length_or_percentage y1;
+	svg_length_or_percentage y2;
+	VkvgPattern pattern;
+}svg_element_linear_gradient;
+
+uint32_t _get_element_hash (void* elt) {
+	return ((svg_element_id*)elt)->hash;
+}
+svg_element_type _get_element_type (void* elt) {
+	return ((svg_element_id*)elt)->type;
+}
+typedef struct {
+	float x;
+	float y;
+	float w;
+	float h;
+} svg_viewbox;
+
+typedef struct {
+	char		elt[128];
+	char		att[1024];
+	char		value[1024*1024];
+	VkvgDevice	dev;
+	VkvgContext	ctx;
+	VkvgSurface surf;
+	uint32_t	width;//force surface width & height
+	uint32_t	height;
+	svg_viewbox viewBox;
+	bool		preserveAspectRatio;
+	bool		skip;//skip tag and children
+	uint32_t	currentIdHash;
+	array_t*	idList;
+	/*long		currentTagStartPos;*/
+
+} svg_context;
+
+enum prevCmd {none, quad, cubic};
+
+int read_tag (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill);
+
+#define get_attribute fscanf(f, " %[^=>]=%*[\"']%[^\"']%*[\"']", svg->att, svg->value)
+
+#define read_tag_end \
+	if (res < 0) {												\
+		LOG("error parsing: %s\n", svg->att);					\
+		return -1;												\
+	}															\
+	if (res == 1) {												\
+		if (getc(f) != '>') {									\
+			LOG("parsing error, expecting '>'\n");				\
+			return -1;											\
+		}														\
+		return 0;												\
+	}															\
+	if (getc(f) != '>') {										\
+		LOG("parsing error, expecting '>'\n");					\
+		return -1;												\
+	}
+
+int skip_attributes_and_children (svg_context* svg, FILE* f, svg_paint_type hasStroke, svg_paint_type hasFill, uint32_t stroke, uint32_t fill);
+
+#define read_element_start										\
+	res = fscanf(f, " <%[^> \n\r]", svg->elt);					\
+	if (res < 0)												\
+		return 0;												\
+	if (!res) {													\
+		res = fscanf(f, "%*[^<]<%[^> \n\r]", svg->elt);			\
+		if (!res) {												\
+			LOG("element name parsing error (%s)\n", svg->elt);	\
+			return -1;											\
+		}														\
+	}															\
+	if (svg->elt[0] == '/') {									\
+		if (getc(f) != '>') {									\
+			LOG("parsing error, expecting '>'\n");				\
+			return -1;											\
+		}														\
+		return 0;												\
+	}															\
+	if (svg->elt[0] == '!') {									\
+		if (!strcmp(svg->elt,"!DOCTYPE")) {						\
+			while (!feof (f))									\
+				if (fgetc(f)=='>')								\
+					break;										\
+		} if (!strncmp(svg->elt, "!--", 3)) {					\
+			while (!feof (f)) {									\
+				if (fgetc(f)=='-') {							\
+					if (fgetc(f)=='-' && fgetc(f)=='>')			\
+						break;									\
+					else										\
+						fseek (f, -1, SEEK_CUR);				\
+				}												\
+			}													\
+		}														\
+		continue;												\
+	}															\
+	if (svg->skip) {											\
+		res = skip_attributes_and_children (svg, f, hasStroke, hasFill, stroke, fill);\
+		continue;												\
+	}															\
+
+#define skip_element {																\
+	svg->skip = true;																\
+	res = skip_attributes_and_children (svg, f, hasStroke, hasFill, stroke, fill);	\
+	svg->skip = false;																\
+}
+
+#define read_attributes_loop_start_lite \
+	while (style || res > 0) {														\
+		if (!style && res == 1 && svg->att[0] == '/')								\
+			break;																	\
+		if (!strcasecmp(svg->att, "style")) {										\
+			int styleLength = strlen (svg->value);									\
+			if (styleLength > 0) {													\
+				style = (char*)malloc(styleLength);									\
+				memccpy(style, svg->value, 0, styleLength);							\
+				pfStyle = fmemopen(style, styleLength, "r");						\
+				res = fscanf(pfStyle, " %[^:]:%[^;];", svg->att, svg->value);		\
+				continue;															\
+			}																		\
+		}																			\
+
+#define read_attributes_loop_start \
+	FILE* pfStyle = NULL;															\
+	char* style = NULL;																\
+	int res = get_attribute;														\
+	read_attributes_loop_start_lite
+
+#define read_attributes_loop_end \
+	if (style) {																	\
+		if (feof(pfStyle)) {														\
+			fclose (pfStyle);														\
+			free (style);															\
+			style = NULL;															\
+		} else {																	\
+			res = fscanf(pfStyle, " %[^:]:%[^;];", svg->att, svg->value);			\
+			continue;																\
+		}																			\
+	}																				\
+	res = get_attribute;															\
+}
+
+#endif // VKVG_SVG_INTERNAL_H
